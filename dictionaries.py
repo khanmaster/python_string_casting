b40002054a9ae5ce5993e5b980064a394f5cac1d
khanmaster <eashkhan@hotmail.com>
2020-07-01 14:40:33 +0100

    dictionaries added

---
+++ dictionaries.py
@@ -0,0 +1,57 @@
+# What is a Dictionary - Data collection
+# Bit more dynamic than tuples and lists
+# how? very useful tool within any coding
+# simple concept of KEY VALUE PAIRS
+
+# Syntax - we use {} to create a Dictionary
+
+# Let's now create an example to learn by doing
+                #   key:      value
+student_record = { "name": "shahrukh",
+                   "stream": "DevOps",
+                   "completed_lesson": 5,
+                   "completed_lessons_names": ["strings", "Tuples", "variables"]
+}
+# display the value of name
+
+print(student_record["name"]) # fetch the value of stream
+
+# fetch the specific index data
+print(student_record["completed_lessons_names"][2])
+
+# how can we remove a value in a list
+
+# student_record["completed_lessons_names"] = 3
+# new_list = student_record["completed_lessons_names"]
+# print(new_list)
+
+
+# add 2 topics that we have covered number Lists
+
+# number 2 Builtin methods
+
+# display the list
+
+
+
+
+
+
+
+
+
+# print(student_record.items())
+# print(student_record["completed_lessons_names"].insert(0, ""))
+
+#print(student_record)
+#print(type(student_record))
+
+#print(sorted(student_record))
+
+#print(student_record.values())
+
+
+
+# print(sorted(str(student_record.values())))
+
+
